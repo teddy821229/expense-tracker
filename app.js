d91e644d7500b3e26a35147b84771135f3759403
teddy <a21506qoo@gmail.com>
2021-05-13 22:56:39 +0800

    feat: add handlebars and render default page success && add static files

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const exphbs = require("express-handlebars")
+
+const port = 3000
 
 // db connection
 mongoose.connect('mongodb://localhost/expense-data', { useNewUrlParser: true, useUnifiedTopology: true })
-
 const db = mongoose.connection
 
 db.on('error', () => {
@@ -14,13 +16,17 @@ db.on('error', () => {
 db.once('open', () =>{
   console.log('mongodb CONNECTED')
 })
-
-
-const port = 3000
+// 設定靜態檔案
+app.use(express.static('public'))
+// set view engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.send('this is expense-tracker project initial.')
+  res.render('index')
 })
+
+
 
 app.listen(port, () =>{
   console.log(`app is running on localhost:${port}`)
