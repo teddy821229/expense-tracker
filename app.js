b29339c1a192e97611a113cef50e40027e9009b5
teddy <a21506qoo@gmail.com>
2021-05-14 18:06:21 +0800

    feat: add new function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require("express-handlebars")
+const bodyParser = require('body-parser')
 const Record = require('./models/record')
+const record = require('./models/record')
 
 const app = express()
 const port = 3000
@@ -19,9 +21,14 @@ db.once('open', () =>{
 })
 // 設定靜態檔案
 app.use(express.static('public'))
+
 // set view engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
+
+// set body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // setting route
 app.get('/', (req, res) => {
   Record.find()
@@ -31,9 +38,27 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+app.get('/records/new', (req, res) => {
+  res.render('new')
+})
 
+app.post('/records/new' ,(req, res) => {
+  return Record.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
+
+app.get('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  Record.findById(id)
+    .lean()
+    .then(record => res.render('edit', { record }))
+    .catch(error => console.error(error))
+})
 
 app.listen(port, () =>{
   console.log(`app is running on localhost:${port}`)
 })
+
+
 
